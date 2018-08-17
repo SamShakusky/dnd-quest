a13100fc80b1edede6eb180788ff239b1f3af727
Eugene <shakuskys@gmail.com>
2018-08-17 09:32:14 +0300

    redux devtools fix

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__;
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
 
 const initialState = {};
 
