86e78cc335f79e78535f2a392e8dd425a7396a6d
Eugene <shakuskys@gmail.com>
2018-10-19 00:06:09 +0300

    split alpha form

--- src/components/alpha.js
+++ src/components/alpha.js
@@ -1,5 +1,10 @@
 import React, { PureComponent } from 'react';
 
+import Formsy from 'formsy-react';
+
+import Button from './button';
+import TextField from './text-field';
+
 import '../css/alpha.css';
 
 export default class Alpha extends PureComponent {
@@ -12,10 +17,44 @@ export default class Alpha extends PureComponent {
         <main
           styleName="container"
         >
-          <h1 styleName="title">
-            Adventure Companion
-            <span styleName="sign">α</span>
-          </h1>
+          <div styleName="title__wrap">
+            <p styleName="title">Adventure</p>
+            <p styleName="title">
+              Companion
+              <span styleName="sign">α</span>
+            </p>
+            
+          </div>
+          <div styleName="form__wrap">
+            <div styleName="form__title">
+              <p>Join</p>
+              <p>Closed Alpha</p>
+            </div>
+            <p styleName="party__title">Create a party</p>
+            <Formsy
+              styleName="form"
+            >
+              <div>
+                <TextField
+                  label="Username"
+                  name="username"
+                  validations={{
+                    minLength      : 4,
+                    maxLength      : 20,
+                    isAlphanumeric : true
+                  }}
+                  validationErrors={{
+                    minLength      : 'Must be between 4 and 20 characters',
+                    maxLength      : 'Must be between 4 and 20 characters',
+                    isAlphanumeric : 'Must only contain letters or numbers'
+                  }}
+                  message="Only letters and numbers"
+                  required
+                />
+              </div>
+              <Button label="Submit" type="submit" shape="ghost" />
+            </Formsy>
+          </div>
         </main>
       );
     }
